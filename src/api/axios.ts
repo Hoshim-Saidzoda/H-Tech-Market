49142ec3065e72b1fb2b0e0279b48757012b96d1
s <hosimsaidzoda@gmail.com>
2025-12-23 17:35:31 +0500

    deploy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,22 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://37.27.29.18:8002", 
+  baseURL: "https://store-api.softclub.tj/", 
   headers: {
     "Content-Type": "application/json",
   },
-});
-
-
-API.interceptors.request.use((config) => {
-  const user = localStorage.getItem("user");
-  if (user) {
-    const { token } = JSON.parse(user);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  return config;
 });
 
 export default API;
--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -10,21 +10,31 @@ export interface GetProductsParams {
 
  
  
-export const getProducts = async (
-  params?: GetProductsParams
-): Promise<Product[]> => {
-  const { data } = await API.get("/Product/get-products", { params });
-  return data.data.products;
+// Получить все продукты
+export const getProducts = async () => {
+  const { data } = await API.get("/Product/get-products", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiIwZGJlZjEzZS01ODQ3LTQ5YzUtYmUwMC0xZDFhMmEzMWNiNzEiLCJuYW1lIjoiaG9zaGltam9ubm4iLCJlbWFpbCI6Imhvc2hpbUBnbWFpbDE5NjY2Ni5jb20iLCJzdWIiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJVc2VyIiwiZXhwIjoxNzY2NDU1MzA1LCJpc3MiOiJvbmxpbmUtc3RvcmUudGoiLCJhdWQiOiJvbmxpbmUtc3RvcmUudGoifQ.JvdFQ0bvbWdFMxvaTHRmWmor27nfnNJZo-juQy-VmOo",
+    },
+  });
+  console.log("API RESPONSE:", data);
+  return data.data.products;  
 };
 
-export const getProductById = async (id: number): Promise<Product> => {
-  const { data } = await API.get(
-    `/Product/get-product-by-id?id=${id}`
-  );
+// Получить продукт по ID
+export const getProductById = async (id: number) => {
+  const { data } = await API.get(`/Product/get-product-by-id?id=${id}`, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiIwZGJlZjEzZS01ODQ3LTQ5YzUtYmUwMC0xZDFhMmEzMWNiNzEiLCJuYW1lIjoiaG9zaGltam9ubm4iLCJlbWFpbCI6Imhvc2hpbUBnbWFpbDE5NjY2Ni5jb20iLCJzdWIiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJVc2VyIiwiZXhwIjoxNzY2NDU1MzA1LCJpc3MiOiJvbmxpbmUtc3RvcmUudGoiLCJhdWQiOiJvbmxpbmUtc3RvcmUudGoifQ.JvdFQ0bvbWdFMxvaTHRmWmor27nfnNJZo-juQy-VmOo",
+    },
+  });
+  console.log("getProductById response:", data);
   return data.data;
 };
 
-
+// Добавить продукт (для админа)
 export const addProduct = async (product: any) => {
   const { data } = await API.post("/Product/add-product", product);
   return data;
